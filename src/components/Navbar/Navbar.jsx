1bf14462bfc2bbcb7c30e97e68c0852f7dea74a7
Makomborero Mangwiro <makomboreroman@gmail.com>
2023-05-28 15:29:07 +1000

    Hideable navnar deployment

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -5,10 +5,10 @@ import '/src/pages/LandingPage/styles/landing_global.css'
 const Navbar = () => {
   return (
     <>
-      <div className=' fixed z-30 navbar-wrapper flex place-content-center place-items-center h-24 w-screen mt-11'>
-        <div className='navbar  max-h-28 pl-5 pr-10 bg-white rounded-full  flex place-content-center place-items-center'>
+      <div className=' fixed z-30 navbar-wrapper flex place-content-center  place-items-center h-24 w-screen mt-2'>
+        <div className='navbar hidable max-h-28 pl-5 pr-10 bg-white rounded-full  flex place-content-center place-items-center'>
           <Navbar_Logo src='earth.png' />
-          <div className='nav-bar-items  flex place-content-start gap-4 overflow-y-hidden overflow-x-auto w-auto'>
+          <div className='nav-bar-items hidable  flex place-content-start gap-4 overflow-y-hidden overflow-x-auto w-auto'>
             <Navbar_Item label='Home' />
             <Navbar_Item label='Experience' />
             <Navbar_Item label='Projects' />
