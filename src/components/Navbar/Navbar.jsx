1161f0b0dbf068d041c2bf71f23649affb724d6f
Makomborero Mangwiro <makomboreroman@gmail.com>
2023-05-30 12:21:49 +1000

    Navbar styling complete

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,14 +1,48 @@
 import Navbar_Logo from './Navbar_Logo'
 import Navbar_Item from './Navbar_Item'
 import '/src/pages/LandingPage/styles/landing_global.css'
+import { useState } from 'react'
+import classNames from 'classnames'
 
 const Navbar = () => {
+  const [hidable, setHidable] = useState(false)
+
+  window.onscroll = () => {
+    if (window.scrollY > 300) {
+      setHidable(true)
+    } else {
+      setHidable(false)
+    }
+  }
+
+  const navbarClass = classNames(
+    'navbar',
+    'max-h-28',
+    'bg-white',
+    'rounded-full',
+    'flex',
+    'place-content-center',
+    'place-items-center',
+    { hidable: hidable }
+  )
+
+  const navBarItemClass = classNames(
+    'nav-bar-items',
+    'flex',
+    'place-content-start',
+    'gap-4',
+    'overflow-y-hidden',
+    'overflow-x-auto',
+    'w-auto',
+    { hidable: hidable }
+  )
+
   return (
     <>
       <div className=' fixed z-30 navbar-wrapper flex place-content-center  place-items-center h-24 w-screen mt-2'>
-        <div className='navbar hidable max-h-28 bg-white rounded-full  flex place-content-center place-items-center'>
+        <div className={navbarClass}>
           <Navbar_Logo src='earth.png' />
-          <div className='nav-bar-items hidable  flex place-content-start gap-4 overflow-y-hidden overflow-x-auto w-auto'>
+          <div className={navBarItemClass}>
             <Navbar_Item label='Home' />
             <Navbar_Item label='Experience' />
             <Navbar_Item label='Projects' />
